cc21f774273b4f11f1063cea7c3befb42645513f
Rupert spelled with one P <g00423530@atu.ie>
2024-11-05 11:00:45 +0000

    Added PathVariables for getEmployee, updateEmployee, and deleteEmployee

--- src/main/java/ie/atu/labexam/employeeController.java
+++ src/main/java/ie/atu/labexam/employeeController.java
@@ -1,29 +1,30 @@
 package ie.atu.labexam;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class employeeController {
     @PostMapping("/employees")
-    public String addEmployee() {
+    public String addEmployee(@Valid @RequestBody employee employeeRequest) {
         return "Employee details";
     }
 
     @GetMapping("/employees/{employeeCode}")
-    public String getEmployee() {
+    public String getEmployee(@PathVariable String employeeCode) {
         // Get employee details from ID
         return "Employee details";
     }
 
     @PutMapping("/employees/{employeeCode}")
-    public String updateEmployee() {
+    public String updateEmployee(@PathVariable String employeeCode) {
         // Delete employee
         // Add employee
         return "Employee details";
     }
 
     @DeleteMapping("/employees/{employeeCode}")
-    public String deleteEmplolyee() {
+    public String deleteEmployee(@PathVariable String employeeCode) {
         // Delete employee
         return "Employee details";
     }
